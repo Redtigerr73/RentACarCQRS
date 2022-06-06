49310f82587ccdc96497244fcf909774a0224ad6
Francisco <Montilla2808@gmail.com>
2022-06-06 16:56:08 +0200

    Role assignment implemented WORKS

--- WebUI.MVC/Models/UserRole.cs
+++ WebUI.MVC/Models/UserRole.cs
@@ -1,10 +1,19 @@
-﻿namespace WebUI.MVC.Models
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WebUI.MVC.Models
 {
     public class UserRole
     {
 
         public string Id { get; set; }
+        [JsonIgnore]
         public string Name { get; set; }
+        [JsonIgnore]
         public string Description { get; set; }
+    }
+    public class UserRoles
+    {
+        public IList<string> roles { get; set; } = new List<string>();
     }
 }
