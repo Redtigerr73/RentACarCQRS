ddef49ec278136b0468555c14610c713132a9721
Francisco <Montilla2808@gmail.com>
2022-06-17 15:02:21 +0200

    fluentValidation correction

--- Application/Features/Bookings/Commands/CreateBookingCommandValidator.cs
+++ Application/Features/Bookings/Commands/CreateBookingCommandValidator.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Application.Bookings.Commands
 {
-    public class CreateBookingCommandValidator : AbstractValidator<Booking>
+    public class CreateBookingCommandValidator : AbstractValidator<CreateBookingCommand>
     {
         private readonly IApplicationDbContext _context;
 
@@ -16,28 +16,31 @@ namespace Application.Bookings.Commands
         {
             _context = context;
             RuleFor(b => b.FromDateTime)
-            .NotNull().WithMessage("Required")
+            .NotNull()
+            .NotEmpty().WithMessage("Required")
             .LessThan(DateTime.Now.ToLocalTime()).WithMessage("Booking can't start in the past");
 
             RuleFor(b => b.ReturnDateTime)
-            .NotNull().WithMessage("Required")
+            .NotNull()
+            .NotEmpty().WithMessage("Required")
             .GreaterThan(b => b.FromDateTime).WithMessage("Return date must be after pick up date");
 
             RuleFor(b => b.CarId)
-            .NotNull().WithMessage("Car Id is Required");
+            .NotNull()
+            .NotEmpty().WithMessage("Car Id is Required");
 
             RuleFor(b => b.CustomerId)
-            .NotNull().WithMessage("Customer Id is Required");
+            .NotNull()
+            .NotEmpty().WithMessage("Customer Id is Required");
 
-            RuleFor(b => b.PickUpLocationId)
-            .NotNull().WithMessage("Pick-up location is Required")
-            .When(b => b.PickUpLocationId != b.Car.LocationId).WithMessage("Mismatch with current car's location");
 
             RuleFor(b => b.DropOffLocationId)
-            .NotNull().WithMessage("Drop-off location is Required");
+            .NotNull()
+            .NotEmpty().WithMessage("Drop-off location is Required");
 
             RuleFor(b => b.PackageId)
-            .NotNull().WithMessage("Package Id is Required");
+            .NotNull()
+            .NotEmpty().WithMessage("Package Id is Required");
         }
 
 
